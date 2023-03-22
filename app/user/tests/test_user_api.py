b9cee3bd87500bcdc96f85636283ae60656e8f7d
adrian_s_lps <adriansoareslps@gmail.com>
2023-03-22 10:25:42 -0300

    Fix email typo in test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,6 +11,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse("user:create")
+GET_TOKEN_URL = reverse("user:token")
 
 
 def create_user(**params):
@@ -25,7 +26,7 @@ class PublicUserAPITests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.payload = {
+        self.user_data_payload = {
             "email": "test@example.com",
             "password": "pass123",
             "name": "User Name",
@@ -34,27 +35,33 @@ class PublicUserAPITests(TestCase):
     def test_create_user_success(self):
         """Test the successful user creation cases."""
 
-        res = self.client.post(CREATE_USER_URL, self.payload)
+        res = self.client.post(CREATE_USER_URL, self.user_data_payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
-        user = get_user_model().objects.get(email=self.payload["email"])
-        self.assertTrue(user.check_password(self.payload["password"]))
+        user = get_user_model().objects.get(
+                email=self.user_data_payload["email"])
+
+        self.assertTrue(user.check_password(
+            self.user_data_payload["password"]))
+
         self.assertNotIn("password", res.data)
 
     def test_error_when_user_exists(self):
         """Test error is returned if user with email exists."""
 
-        create_user(**self.payload)
+        create_user(**self.user_data_payload)
 
-        res = self.client.post(CREATE_USER_URL, self.payload)
+        res = self.client.post(CREATE_USER_URL, self.user_data_payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
         """Test that created password less than 5 raises error."""
 
-        ch_payload = self.payload.copy()
-        ch_payload["password"] = "ps"
+        ch_payload = {
+            'email': 'wrong@example.test',
+            'password': "ps",
+        }
 
         res = self.client.post(CREATE_USER_URL, ch_payload)
 
@@ -64,3 +71,47 @@ class PublicUserAPITests(TestCase):
         )
 
         self.assertFalse(check_created)
+
+    def test_token_session_creation(self):
+        """Test generates token for valid credentials."""
+
+        user_data_payload = {
+            'name': 'Test Name',
+            'email': 'test@example.com',
+            'password': 'test-user-password123',
+        }
+
+        auth_payload = {
+            "email": user_data_payload["email"],
+            "password": user_data_payload["password"],
+        }
+
+        create_user(**user_data_payload)
+
+        res = self.client.post(GET_TOKEN_URL, auth_payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error for invalid credentials."""
+
+        create_user(**self.user_data_payload)
+        wrong_payload = {"email": "wrong@email.com", "password": "aabbcc"}
+        res = self.client.post(GET_TOKEN_URL, wrong_payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_return_error_for_blank_password(self):
+        """Test that no blank password should be authenticated."""
+
+        nopass_payload = {
+            "email": self.user_data_payload["email"],
+            "password": ""
+        }
+
+        res = self.client.post(GET_TOKEN_URL, nopass_payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertTrue(res.status_code, status.HTTP_400_BAD_REQUEST)
